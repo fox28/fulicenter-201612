63525f9f8a9ec98fd0a93aab51593150b830e090
Fox <279099862@qq.com>
2017-03-27 23:19:53 +0800

    GoodsAdapter显示图片

---
+++ app/src/main/java/com/example/apple/fulicenter/ui/adapter/GoodsAdapter.java
@@ -0,0 +1,69 @@
+package com.example.apple.fulicenter.ui.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.apple.fulicenter.R;
+import com.example.apple.fulicenter.model.bean.NewGoodsBean;
+import com.example.apple.fulicenter.model.utils.ImageLoader;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by apple on 2017/3/27.
+ */
+
+public class GoodsAdapter extends RecyclerView.Adapter {
+    Context context;
+    List<NewGoodsBean> mList;
+
+    public GoodsAdapter(Context context, List<NewGoodsBean> list) {
+        this.context = context;
+        this.mList = list;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        GoodsViewHolder holder = new GoodsViewHolder(View.inflate(context, R.layout.item_goods, null));
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder parentHolder, int position) {
+        GoodsViewHolder holder = (GoodsViewHolder) parentHolder;
+        NewGoodsBean bean = mList.get(position);
+        holder.tvGoodsName.setText(bean.getGoodsName());
+        holder.tvGoodsPrice.setText(bean.getCurrencyPrice());
+        ImageLoader.downloadImg(context, holder.ivGoodsThumb,bean.getGoodsThumb());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList != null ? mList.size() : 0;
+    }
+
+    class GoodsViewHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.ivGoodsThumb)
+        ImageView ivGoodsThumb;
+        @BindView(R.id.tvGoodsName)
+        TextView tvGoodsName;
+        @BindView(R.id.tvGoodsPrice)
+        TextView tvGoodsPrice;
+        @BindView(R.id.layout_goods)
+        LinearLayout layoutGoods;
+
+        GoodsViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+}
