3b943e7533d6958461f52e2301bcfb6ee1bc986e
Fox <279099862@qq.com>
2017-04-01 13:40:11 +0800

    优化分类首页的用户体验

--- app/src/main/java/com/example/apple/fulicenter/ui/fragment/CategoryFragment.java
+++ app/src/main/java/com/example/apple/fulicenter/ui/fragment/CategoryFragment.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ExpandableListView;
+import android.widget.LinearLayout;
 
 import com.example.apple.fulicenter.R;
 import com.example.apple.fulicenter.model.bean.CategoryChildBean;
@@ -23,6 +24,7 @@ import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
 /**
@@ -40,6 +42,10 @@ public class CategoryFragment extends Fragment {
     @BindView(R.id.elv)
     ExpandableListView mElv;
     Unbinder unbinder;
+    @BindView(R.id.layout_tips)
+    LinearLayout mLayoutTips;
+    View loadView;
+    View loadFail;
 
 
     @Nullable
@@ -47,6 +53,14 @@ public class CategoryFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_category, container, false);
         unbinder = ButterKnife.bind(this, view);
+        loadView = LayoutInflater.from(getContext()).inflate(R.layout.loading, mLayoutTips, false);
+        loadFail = LayoutInflater.from(getContext()).inflate(R.layout.load_fail, mLayoutTips, false);
+        mLayoutTips.addView(loadView); // !!!切记别忘记
+        mLayoutTips.addView(loadFail);
+        // loadView显示
+
+        showDialog(true, false);
+
         return view;
     }
 
@@ -74,13 +88,14 @@ public class CategoryFragment extends Fragment {
                     groupList.addAll(list);
                     for (int i = 0; i < list.size(); i++) {
                         childList.add(new ArrayList<CategoryChildBean>());
-                        loadChildData(list.get(i).getId(),i);
+                        loadChildData(list.get(i).getId(), i);
                     }
                 }
             }
 
             @Override
             public void onError(String error) {
+                showDialog(false, false);// loadView消失1
 
             }
         });
@@ -93,34 +108,66 @@ public class CategoryFragment extends Fragment {
                 loadIndex++;
                 if (result != null) {
                     ArrayList<CategoryChildBean> list = ResultUtils.array2List(result);
-                    childList.set(index,list);
+                    childList.set(index, list);
                 }
                 if (loadIndex == groupList.size()) {
                     mAdapter.initData(groupList, childList);
 
                     L.e(TAG, "标记1：load and data ... ");
+                    showDialog(false, true);
+
                 }
 
             }
 
             @Override
             public void onError(String error) {
+
                 loadIndex++;
-                L.e(TAG, "标记2：loadChildData,"+error);
+//                L.e(TAG, "标记2：loadChildData," + error);
 
                 if (loadIndex == groupList.size()) {
-                    mAdapter.initData(groupList, childList);
 
-                    L.e(TAG, "标记3：load and data ... ");
+                    mAdapter.initData(groupList, childList);
+                    showDialog(false, false);
+//                    L.e(TAG, "标记3：load and data ... ");
+
                 }
+
 
             }
         });
     }
+
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         unbinder.unbind();
     }
+
+    @OnClick(R.id.layout_tips)
+    public void loadAgain() {
+        loadGroupData();
+        showDialog(true, false);
+
+    }
+
+    /**
+     * @param dialog  是否显示dialog
+     * @param success 是否加载成功
+     */
+    private void showDialog(boolean dialog, boolean success) {
+        loadView.setVisibility(dialog ? View.VISIBLE : View.GONE);
+        if (dialog) {
+            mLayoutTips.setVisibility(View.VISIBLE);
+            loadFail.setVisibility(View.GONE);
+        } else {
+            mLayoutTips.setVisibility(success ? View.GONE : View.VISIBLE);
+            loadFail.setVisibility(success ? View.GONE : View.VISIBLE);
+
+        }
+    }
+
 }
+
