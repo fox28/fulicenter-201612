779b2cbb7bf1ab11caa902747810978965232691
Fox <279099862@qq.com>
2017-04-06 01:37:55 +0800

    用户注册后将用户名返回给登录界面

--- app/src/main/java/com/example/apple/fulicenter/ui/activity/LoginActivity.java
+++ app/src/main/java/com/example/apple/fulicenter/ui/activity/LoginActivity.java
@@ -1,12 +1,16 @@
 package com.example.apple.fulicenter.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 import com.example.apple.fulicenter.R;
+import com.example.apple.fulicenter.application.I;
 import com.example.apple.fulicenter.ui.view.MFGT;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
@@ -14,6 +18,11 @@ import butterknife.OnClick;
  * Created by apple on 2017/4/4.
  */
 public class LoginActivity extends AppCompatActivity {
+
+    @BindView(R.id.et_username)
+    EditText mEtUsername;
+    @BindView(R.id.et_password)
+    EditText mEtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +50,14 @@ public class LoginActivity extends AppCompatActivity {
     @OnClick(R.id.backClickArea)
     public void onBackClicked() {
         MFGT.finish(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == I.REQUEST_CODE_REGISTER) {
+            String username = data.getStringExtra(I.User.USER_NAME);
+            mEtUsername.setText(username);
+        }
     }
 }
