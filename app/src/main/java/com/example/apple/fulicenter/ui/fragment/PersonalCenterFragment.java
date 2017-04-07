3056eafb6fbd94fdff95c00245e4a3953a7b0c31
Fox <279099862@qq.com>
2017-04-07 18:47:54 +0800

    个人中心添加orderList

--- app/src/main/java/com/example/apple/fulicenter/ui/fragment/PersonalCenterFragment.java
+++ app/src/main/java/com/example/apple/fulicenter/ui/fragment/PersonalCenterFragment.java
@@ -6,7 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.example.apple.fulicenter.R;
@@ -15,6 +17,9 @@ import com.example.apple.fulicenter.model.bean.User;
 import com.example.apple.fulicenter.model.utils.ImageLoader;
 import com.example.apple.fulicenter.model.utils.L;
 import com.example.apple.fulicenter.ui.view.MFGT;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -35,6 +40,8 @@ public class PersonalCenterFragment extends Fragment {
     TextView mTvCollectCount;
     Unbinder unbinder;
     User mUser;
+    @BindView(R.id.center_user_order_lis)
+    GridView mCenterUserOrderLis;
 
     @Nullable
     @Override
@@ -52,14 +59,15 @@ public class PersonalCenterFragment extends Fragment {
             MFGT.gotoLoginActivity(getActivity());
         } else {
             showUserInfo();
+            initOrderList();
         }
 
     }
 
     private void showUserInfo() {
         mTvUserName.setText(mUser.getMuserNick());
-        ImageLoader.downloadImg(getContext(),mIvUserAvatar,mUser.getAvatar());
-        L.e(TAG, "mUser.getAvatar = "+ mUser.getAvatar());
+        ImageLoader.downloadImg(getContext(), mIvUserAvatar, mUser.getAvatar());
+        L.e(TAG, "mUser.getAvatar = " + mUser.getAvatar());
     }
 
     @Override
@@ -67,4 +75,35 @@ public class PersonalCenterFragment extends Fragment {
         super.onDestroyView();
         unbinder.unbind();
     }
+
+    private void initOrderList() {
+        ArrayList<HashMap<String, Object>> data = new ArrayList<>();
+        HashMap<String, Object> order1 = new HashMap<>();
+        order1.put("order", R.drawable.order_list1);
+        data.add(order1);
+
+        HashMap<String, Object> order2 = new HashMap<>();
+        order2.put("order", R.drawable.order_list2);
+        data.add(order2);
+
+        HashMap<String, Object> order3 = new HashMap<>();
+        order3.put("order", R.drawable.order_list3);
+        data.add(order3);
+
+        HashMap<String, Object> order4 = new HashMap<>();
+        order4.put("order", R.drawable.order_list4);
+        data.add(order4);
+
+        HashMap<String, Object> order5 = new HashMap<>();
+        order5.put("order", R.drawable.order_list5);
+        data.add(order5);
+
+        SimpleAdapter adapter = new SimpleAdapter(getContext(), data, R.layout.simple_adapter,
+                new String[]{"order"}, new int[]{R.id.iv_order});
+        mCenterUserOrderLis.setAdapter(adapter);
+
+
+    }
+
+
 }
