29fbc8617877a80df2afe35047821b76081b865f
Fox <279099862@qq.com>
2017-04-07 12:14:35 +0800

    闪屏界面获取数据库的用户信息

--- app/src/main/java/com/example/apple/fulicenter/ui/activity/SplashActivity.java
+++ app/src/main/java/com/example/apple/fulicenter/ui/activity/SplashActivity.java
@@ -1,12 +1,16 @@
 package com.example.apple.fulicenter.ui.activity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.apple.fulicenter.R;
+import com.example.apple.fulicenter.application.FuLiCenterApplication;
+import com.example.apple.fulicenter.model.bean.User;
+import com.example.apple.fulicenter.model.dao.UserDao;
+import com.example.apple.fulicenter.model.utils.L;
+import com.example.apple.fulicenter.model.utils.SharePreferenceUtils;
 import com.example.apple.fulicenter.ui.view.MFGT;
 
 /**
@@ -14,11 +18,11 @@ import com.example.apple.fulicenter.ui.view.MFGT;
  */
 
 public class SplashActivity extends AppCompatActivity {
+    private static final String TAG = SplashActivity.class.getSimpleName();
     int time = 2000;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_splash);
     }
 
@@ -28,10 +32,14 @@ public class SplashActivity extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-
-//                startActivity(new Intent(SplashActivity.this, MainActivity.class));
-// 跟下面作用相同
-
+                String username = SharePreferenceUtils.getInstance().getUsername();
+                if (username != null) {
+                    User user = UserDao.getInstance(SplashActivity.this).getUserInfo(username);
+                    L.e(TAG, "onStart, user = "+user);
+                    FuLiCenterApplication.setCurrentUser(user);
+                }
+                //startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                // 跟下面作用相同
                 MFGT.gotoMainActivity(SplashActivity.this);
                 SplashActivity.this.finish();
 
