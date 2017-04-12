d521c2db209c7ba036e507a2e903b84bafc2086e
Fox <279099862@qq.com>
2017-04-12 10:46:29 +0800

    解决bug：意外退出(系统被kill，空指针等)后，内存用户数据被清空、用户需要重新登录——这种情况下需要去数据库获取数据并填充到内存中

--- app/src/main/java/com/example/apple/fulicenter/application/FuLiCenterApplication.java
+++ app/src/main/java/com/example/apple/fulicenter/application/FuLiCenterApplication.java
@@ -4,6 +4,9 @@ import android.app.Application;
 import android.content.Context;
 
 import com.example.apple.fulicenter.model.bean.User;
+import com.example.apple.fulicenter.model.dao.UserDao;
+import com.example.apple.fulicenter.model.utils.L;
+import com.example.apple.fulicenter.model.utils.SharePreferenceUtils;
 
 /**
  * Created by apple on 2017/3/27.
@@ -15,6 +18,16 @@ public class FuLiCenterApplication extends Application {
     static User currentUser;
 
     public static User getCurrentUser() {
+        if (currentUser == null) {
+            final String username = SharePreferenceUtils.getInstance().getUsername();
+            L.e("application", "username = " +username);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    User user = UserDao.getInstance(instance).getUserInfo(username);
+                }
+            }).start(); // 别忘了start()!
+        }
         return currentUser;
     }
 
