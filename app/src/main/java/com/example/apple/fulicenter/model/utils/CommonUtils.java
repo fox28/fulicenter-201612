eab9438313a87e57f403dc55bb2720ff7c777479
Fox <279099862@qq.com>
2017-04-12 16:41:19 +0800

    Toast优化

--- app/src/main/java/com/example/apple/fulicenter/model/utils/CommonUtils.java
+++ app/src/main/java/com/example/apple/fulicenter/model/utils/CommonUtils.java
@@ -1,21 +1,35 @@
 package com.example.apple.fulicenter.model.utils;
 
+import android.content.Context;
 import android.widget.Toast;
 
 import com.example.apple.fulicenter.application.FuLiCenterApplication;
 
 
 public class CommonUtils {
+    private static Toast toast;
+
     public static void showLongToast(String msg){
-        Toast.makeText(FuLiCenterApplication.getInstance(),msg,Toast.LENGTH_LONG).show();
+//        Toast.makeText(FuLiCenterApplication.getInstance(),msg,Toast.LENGTH_LONG).show();
+        showToast(FuLiCenterApplication.getInstance(), msg, Toast.LENGTH_LONG).show();
     }
     public static void showShortToast(String msg){
-        Toast.makeText(FuLiCenterApplication.getInstance(),msg,Toast.LENGTH_SHORT).show();
+//        Toast.makeText(FuLiCenterApplication.getInstance(),msg,Toast.LENGTH_SHORT).show();
+        showToast(FuLiCenterApplication.getInstance(), msg, Toast.LENGTH_SHORT).show();
     }
     public static void showLongToast(int rId){
         showLongToast(FuLiCenterApplication.getInstance().getString(rId));
     }
     public static void showShortToast(int rId){
         showShortToast(FuLiCenterApplication.getInstance().getString(rId));
+    }
+
+    public static Toast showToast(Context context, String msg, int length) {
+        if (toast == null) {
+            toast = Toast.makeText(context, msg, length);
+        } else {
+            toast.setText(msg);
+        }
+        return toast;
     }
 }
