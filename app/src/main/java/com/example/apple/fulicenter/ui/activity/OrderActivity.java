6e7f733a1d1ecce5ee3027e30d533eabca0b6634
Fox <279099862@qq.com>
2017-04-16 22:11:06 +0800

    订单页面的非空验证

--- app/src/main/java/com/example/apple/fulicenter/ui/activity/OrderActivity.java
+++ app/src/main/java/com/example/apple/fulicenter/ui/activity/OrderActivity.java
@@ -3,13 +3,17 @@ package com.example.apple.fulicenter.ui.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.apple.fulicenter.R;
 import com.example.apple.fulicenter.application.I;
 import com.example.apple.fulicenter.ui.view.MFGT;
+
+import org.w3c.dom.Text;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -57,5 +61,35 @@ public class OrderActivity extends AppCompatActivity {
     @OnClick(R.id.tv_order_buy)
     public void orderCommit(){
 
+        String receiveName = mEdOrderName.getText().toString();
+        if (TextUtils.isEmpty(receiveName)) {
+            mEdOrderName.requestFocus();
+            mEdOrderName.setError("收件人姓名不能为空");
+            return;
+        }
+
+        String phone = mEdOrderPhone.getText().toString();
+        if (TextUtils.isEmpty(phone)) {
+            mEdOrderPhone.requestFocus();
+            mEdOrderPhone.setError("手机号码不能为空");
+            return;
+        }
+        if (!phone.matches("1[\\d]{10}")) {
+            mEdOrderPhone.requestFocus();
+            mEdOrderPhone.setError("手机号码格式错误");
+        }
+
+        String area = mSpinOrderProvince.getSelectedItem().toString();
+        if (TextUtils.isEmpty(area)) {
+            Toast.makeText(this, "收货地区不能为空", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        String address = mEdOrderStreet.getText().toString();
+        if (TextUtils.isEmpty(address)) {
+            mEdOrderStreet.requestFocus();
+            mEdOrderStreet.setError("详细地址不能为空");
+            return;
+        }
     }
 }
