26b5f24c165903c498173362d794eade757f7be5
Fox <279099862@qq.com>
2017-04-17 12:39:33 +0800

    订单详情页面增加发起支付功能，有bug待解决

--- app/src/main/java/com/example/apple/fulicenter/ui/activity/OrderActivity.java
+++ app/src/main/java/com/example/apple/fulicenter/ui/activity/OrderActivity.java
@@ -1,5 +1,6 @@
 package com.example.apple.fulicenter.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -11,9 +12,19 @@ import android.widget.Toast;
 
 import com.example.apple.fulicenter.R;
 import com.example.apple.fulicenter.application.I;
+import com.example.apple.fulicenter.model.utils.L;
 import com.example.apple.fulicenter.ui.view.MFGT;
+import com.pingplusplus.android.PingppLog;
+import com.pingplusplus.libone.PaymentHandler;
+import com.pingplusplus.libone.PingppOne;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.w3c.dom.Text;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -25,6 +36,8 @@ import butterknife.OnClick;
 
 public class OrderActivity extends AppCompatActivity {
     int orderPrice;
+    private static String URL = "http://218.244.151.190/demo/charge";
+
 
     @BindView(R.id.tv_common_title)
     TextView mTvCommonTitle;
@@ -46,6 +59,18 @@ public class OrderActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         orderPrice = getIntent().getIntExtra(I.ORDER_BUY_PRICE,0);
         initView();
+        initPay();
+    }
+
+    private void initPay() {
+        //设置需要使用的支付方式
+        PingppOne.enableChannels(new String[] { "wx", "alipay", "upacp", "bfb", "jdpay_wap" });
+
+        // 提交数据的格式，默认格式为json
+        // PingppOne.CONTENT_TYPE = "application/x-www-form-urlencoded";
+        PingppOne.CONTENT_TYPE = "application/json";
+
+        PingppLog.DEBUG = true;
     }
 
     private void initView() {
@@ -91,5 +116,55 @@ public class OrderActivity extends AppCompatActivity {
             mEdOrderStreet.setError("详细地址不能为空");
             return;
         }
+
+        payment();// 设置支付方法
+    }
+
+    private void payment() {
+
+        // 产生个订单号
+        String orderNo = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
+
+            // 计算总金额（以分为单位）
+            int amount = orderPrice*100;
+//        JSONArray billList = new JSONArray();
+//        for (Good good : mList) {
+//            amount += good.getPrice() * good.getCount() * 100;
+//            billList.put(good.getName() + " x " + good.getCount());
+//        }
+        // 构建账单json对象
+        JSONObject bill = new JSONObject();
+
+        // 自定义的额外信息 选填
+        JSONObject extras = new JSONObject();
+        try {
+            extras.put("extra1", "extra1");
+            extras.put("extra2", "extra2");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            bill.put("order_no", orderNo);
+            bill.put("amount", amount);
+            bill.put("extras", extras);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        //壹收款: 创建支付通道的对话框
+        PingppOne.showPaymentChannels(this, bill.toString(), URL, new PaymentHandler() {
+            @Override public void handlePaymentResult(Intent data) {
+                if (data != null) {
+                    /**
+                     * code：支付结果码  -2:服务端错误、 -1：失败、 0：取消、1：成功
+                     * error_msg：支付结果信息
+                     */
+                    int code = data.getExtras().getInt("code");
+                    String result = data.getExtras().getString("result");
+                    L.e("pay", "code = "+code+", result="+result);
+                }
+            }
+        });
     }
 }
